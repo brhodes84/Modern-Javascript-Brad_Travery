1a6eb7834e78ced05c6336b8b5fee5d6acb2400a
Brandon Rhodes <brhodes1984@gmail.com>
2022-12-29 18:58:40 -0600

    Added examples of console.log, var, let, const

---
+++ js_sandbox/app.js
@@ -0,0 +1,26 @@
+// Log to Console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.warn('This is a warning');
+console.clear();
+
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
